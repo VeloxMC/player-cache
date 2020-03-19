0eb4e80f03503103bacdbf2e5e904d29e382a782
Lukas Schulte Pelkum <lukaesebrot@gmail.com>
2020-03-19 23:13:07 +0100

    Implement invalidate endpoint

--- api/endInvalidate.go
+++ api/endInvalidate.go
@@ -4,5 +4,17 @@ import "github.com/valyala/fasthttp"
 
 // endInvalidateHandler handles the '/invalidate' endpoint
 func endInvalidateHandler(ctx *fasthttp.RequestCtx) {
-	// TODO: Implement endpoint handling
+	// Validate the authorization header
+	if !validateAuthorization(ctx) {
+		ctx.SetStatusCode(fasthttp.StatusUnauthorized)
+		ctx.SetBody(errorResponse(errUnauthorized))
+		return
+	}
+
+	// Invalidate the cache
+	cacheInstance.Invalidate()
+
+	// Print a success message
+	ctx.SetStatusCode(fasthttp.StatusOK)
+	ctx.SetBody(successResponse("invalidated cache"))
 }
