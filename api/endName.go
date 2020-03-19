eeb00ea02a4d42aa6cd7dbf9d21b8ec2e44c8c21
Lukas Schulte Pelkum <lukaesebrot@gmail.com>
2020-03-19 23:11:50 +0100

    Implement name endpoint

--- api/endName.go
+++ api/endName.go
@@ -4,6 +4,22 @@ import "github.com/valyala/fasthttp"
 
 // endNameHandler handles the '/name/:uuid' endpoint
 func endNameHandler(ctx *fasthttp.RequestCtx) {
-	// TODO: Implement endpoint handling
+	// Validate the authorization header
+	if !validateAuthorization(ctx) {
+		ctx.SetStatusCode(fasthttp.StatusUnauthorized)
+		ctx.SetBody(errorResponse(errUnauthorized))
+		return
+	}
+
+	// Get the corresponding name
+	name, err := cacheInstance.GetName(ctx.UserValue("uuid").(string)); if err != nil {
+		ctx.SetStatusCode(fasthttp.StatusInternalServerError)
+		ctx.SetBody(errorResponse(err))
+		return
+	}
+
+	// Print the fetched name
+	ctx.SetStatusCode(fasthttp.StatusOK)
+	ctx.SetBody(successResponse(name))
 }
 
